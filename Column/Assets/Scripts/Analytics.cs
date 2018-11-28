3c793ec7ded58ec1c27108abd5197ea2cb8a4ce1
Lewis Nicholson <Lewies@Live.co.uk>
2018-11-28 20:10:20 +0000

    Adding Amplitude

---
+++ Column/Assets/Scripts/Analytics.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Analytics : MonoBehaviour
+{
+    void Awake()
+    {
+        Amplitude amplitude = Amplitude.Instance;
+        amplitude.logging = true;
+        amplitude.init("3f9c7b0d19b01caf443c0a376e6e10b0");
+    }
+}
